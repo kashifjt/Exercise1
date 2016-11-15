38ddf7ff68835859778e78bb26ac42ed5f4bbda9
Kashif Javed <kashifjt@gmail.com>
2016-11-15 15:58:10 +0100

    Added new class HelloUser.java

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,11 @@
+
+public class HelloUser {
+	private String userName;
+	public HelloUser(String inp) {
+		userName = inp;
+	}
+	public void greetUser()
+	{
+		System.out.println("Hello "+userName+"!");
+	}
+}
